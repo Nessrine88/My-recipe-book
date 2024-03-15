76ba3e2da92cfd301016f3797cbc2cc6882e6037
unknown <macherki.nessrine@yahoo.com>
2024-03-15 20:25:24 +0100

    add app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'recipe_foods/new'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +8,16 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get 'public_recipes', to: 'recipes#public_recipes'
+
+  resources :foods
+  resources :users
+  resources :shopping_lists
+  resources :recipes do
+    member do
+      patch 'toggle', to: 'recipes#toggle_recipe'
+    end
+    resources :recipe_foods
+  end
+  root "recipes#index"
 end
